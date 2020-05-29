8b9165b43d4f075a9eb2ecb18518ea1cd1e79167
roxas <cuapopomama200@gmail.com>
2020-05-30 03:05:41 +0800

    使Ios 也可以自動提升BundleVersion

--- Assets/Scripts/Editor/AutoIncreaseBundle.cs
+++ Assets/Scripts/Editor/AutoIncreaseBundle.cs
@@ -8,8 +8,13 @@ class AutoIncreaseBundle : IPreprocessBuildWithReport
     public int callbackOrder { get { return 0; } }
     public void OnPreprocessBuild(BuildReport report)
     {
-        Debug.Log("Auto Increase bundleVersionCode...");
-        PlayerSettings.Android.bundleVersionCode++;
+        Debug.Log("Auto Increase android bundleVersionCode...");
+        int androidBV = PlayerSettings.Android.bundleVersionCode;
+        int iosBV = System.Convert.ToInt32(PlayerSettings.iOS.buildNumber);
+
+        int BV = Mathf.Max(androidBV, iosBV) + 1;
+        PlayerSettings.Android.bundleVersionCode = BV;
+        PlayerSettings.iOS.buildNumber = BV.ToString();
     }
 }
 #endif
