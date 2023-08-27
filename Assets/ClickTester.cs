0e2ec1e60aa11da4aae298fb96471197d9cbe03d
nikki <moniqshiin@gmail.com>
2023-08-27 22:27:04 +0800

    整匯

---
+++ Assets/ClickTester.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickTester : MonoBehaviour, IPointerDownHandler
+{
+    private void OnMouseDown() {
+        Debug.Log("mouse down");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Down");
+    }
+}
