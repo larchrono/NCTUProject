12f13fb8bebbbc38da9f49a16bae2ce11c8ba83c
nikki <moniqshiin@gmail.com>
2023-09-16 00:40:14 +0800

    更新自動Boundary 使其能運作旋轉功能

--- Assets/Scripts/WorkFBX.cs
+++ Assets/Scripts/WorkFBX.cs
@@ -5,8 +5,31 @@ using UnityEngine;
 public class WorkFBX : MonoBehaviour
 {
     public float scaleFactor = 1;
+    public float scaleFactorIOS = 0.05f;
+    public BoxCollider box;
+
     void Start()
     {
+        #if UNITY_IOS
+        scaleFactor = scaleFactorIOS;
+        #endif
         transform.GetChild(0).transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+
+        var mesh = gameObject.GetComponentInChildren<MeshRenderer>();
+
+        if(mesh == null)
+            return;
+
+        var bounds = mesh.bounds;
+
+        // In world-space!
+        var size = bounds.size;
+        var center = bounds.center;
+
+        box.center = center;
+        box.size = size;
+
+        //if(mesh != null)
+        //    mesh.gameObject.AddComponent<BoxCollider>();
     }
 }
